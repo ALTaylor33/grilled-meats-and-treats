70d95d23324375a69be1bf80740e79a969882f2f
ALTaylor33 <austin.taylor33@rocketmail.com>
2023-04-27 19:47:46 -0400

    completed starter code

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const homeRoutes = require('..homeroutes/homeRoutes');
+const projectRoutes = require('./projectRoutes');
 
 router.use('/users', userRoutes);
-router.use('/projects', homeRoutes);
+router.use('/projects', projectRoutes);
 
 module.exports = router;
