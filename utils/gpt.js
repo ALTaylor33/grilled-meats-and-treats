6d75d409b072133aa6c3a2357a9ec9f47947af36
Eric Jacob <explorer121@gmail.com>
2023-05-02 19:46:50 -0400

    Added env file with api keys

--- utils/gpt.js
+++ utils/gpt.js
@@ -1,25 +1,25 @@
 const { Configuration, OpenAIApi } = require('openai');
 
 const config = new Configuration({
-    apiKey: "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" // temp key attached to eric's account. DO NOT SHARE
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(config);
 
 const generateFood = async () => {
 
-    const prompt = "Name a shareable dish to bring to a cookout. Stop after one sentence." //Change this to change AI input (longer is more expensive)
+    const prompt = "Name a shareable dish to bring to a cookout. Stop after one sentence" //Change this to change AI input (longer is more expensive)
 
     const response = await openai.createCompletion({
         model: 'text-curie-001', // language model, look up options to change (current option is cheapest text model)
         prompt: prompt, 
-        max_tokens: 32,   // Output max length (longer is more expensive)
+        max_tokens: 20,   // Output max length (longer is more expensive)
         temperature: 0.5, // randomness setting (can be set between 0 and 1)
     });
 
-    // console.log(response.data.choices[0].text); 
+    console.log(response.data.choices[0].text); 
     return response.data.choices[0].text
 }
 
 
-// generateFood();
+generateFood();
