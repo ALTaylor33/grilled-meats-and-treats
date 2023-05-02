74c11c9838ac91e0cf3cd2461ce52d9f52ae170f
Eric Jacob <explorer121@gmail.com>
2023-05-02 16:34:01 -0400

    added comments for modification

--- utils/gpt.js
+++ utils/gpt.js
@@ -8,13 +8,13 @@ const openai = new OpenAIApi(config);
 
 const generateFood = async () => {
 
-    const prompt = "Give one example of a shareable dish to bring to a cookout. Stop after one sentence."
+    const prompt = "Give one example of a shareable dish to bring to a cookout. Stop after one sentence." //Change this to change AI input (longer is more expensive)
 
     const response = await openai.createCompletion({
-        model: 'text-curie-001', 
+        model: 'text-curie-001', // language model, look up options to change (current option is cheapest text model)
         prompt: prompt, 
-        max_tokens: 32,
-        temperature: 0.5, 
+        max_tokens: 32,   // Output max length (longer is more expensive)
+        temperature: 0.5, // randomness setting (can be set between 0 and 1)
     });
 
     console.log(response.data.choices[0].text); 
