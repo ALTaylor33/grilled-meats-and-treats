f5257533ff1d71393b347d4a9353c934aeaba5cf
chrisg <cleegould@gmail.com>
2023-05-02 21:07:01 -0400

    "lets do this"

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -3,23 +3,24 @@ const { Project, User } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
+  console.log('inside homeroutes')
   try {
-    // Get all projects and JOIN with user data
-    const projectData = await Project.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
-    });
+    // // Get all projects and JOIN with user data
+    // const eventData = await Event.findAll({
+    //   include: [
+    //     {
+    //       model: Guest,
+    //       attributes: ['name'],
+    //     },
+    //   ],
+    // });
 
     // Serialize data so the template can read it
-    const projects = projectData.map((project) => project.get({ plain: true }));
+    const events = eventData.map((event) => event.get({ plain: true }));
 
     // Pass serialized data and session flag into template
     res.render('homepage', { 
-      projects, 
+      events, 
       logged_in: req.session.logged_in 
     });
   } catch (err) {
@@ -53,15 +54,15 @@ router.get('/project/:id', async (req, res) => {
 router.get('/profile', withAuth, async (req, res) => {
   try {
     // Find the logged in user based on the session ID
-    const userData = await User.findByPk(req.session.user_id, {
+    const guestData = await Guest.findByPk(req.session.guest_id, {
       attributes: { exclude: ['password'] },
-      include: [{ model: Project }],
+      include: [{ model: Event }],
     });
 
-    const user = userData.get({ plain: true });
+    const guest = guestData.get({ plain: true });
 
     res.render('profile', {
-      ...user,
+      ...guest,
       logged_in: true
     });
   } catch (err) {
