39f350219c9eb57a536977821ce808b8557742af
Eric Jacob <explorer121@gmail.com>
2023-05-02 21:28:59 -0400

    Replaced broken key and tuned prompt

--- utils/gpt.js
+++ utils/gpt.js
@@ -10,12 +10,12 @@ const openai = new OpenAIApi(config);
 
 const generateFood = async () => {
 
-    const prompt = "Name a shareable dish to bring to a cookout. Stop after one sentence" //Change this to change AI input (longer is more expensive)
+    const prompt = "As a chef, give a single suggestion and basic description of a food apporopriate for sharing at a casual cookout" //Change this to change AI input (longer is more expensive)
 
     const response = await openai.createCompletion({
-        model: 'text-curie-001', // language model, look up options to change (current option is cheapest text model)
+        model: 'text-davinci-002', // language model, look up options to change (current option is cheapest text model)
         prompt: prompt, 
-        max_tokens: 20,   // Output max length (longer is more expensive)
+        max_tokens: 100,   // Output max length (longer is more expensive)
         temperature: 0.5, // randomness setting (can be set between 0 and 1)
     });
 
