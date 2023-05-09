b0deed7113af4245a202edebb345de54d67aa210
Eric Jacob <explorer121@gmail.com>
2023-05-09 16:00:41 -0400

    altered helper format

--- utils/gpt.js
+++ utils/gpt.js
@@ -1,35 +1,30 @@
-const hbs = expbs.create({
+Handlebars.registerHelper(randomFood){
 
-helpers: {
-    randomFood: function() {
+  
 
-      const { Configuration, OpenAIApi } = require('openai');
+  const { Configuration, OpenAIApi } = require('openai');
 
-      require('dotenv').config({path:'../.env'})
+  require('dotenv').config({path:'../.env'})
 
-      const config = new Configuration({
-        apiKey: process.env.OPENAI_API_KEY,
+  const config = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+
+  const openai = new OpenAIApi(config);
+
+  const generateFood = async () => {
+
+      const prompt = "As a chef, give a single suggestion and basic description of a food apporopriate for sharing at a casual cookout" //Change this to change AI input (longer is more expensive)
+
+     const response = await openai.createCompletion({
+          model: 'text-davinci-002', // language model, look up options to change (current option is cheapest text model)
+          prompt: prompt, 
+          max_tokens: 100,   // Output max length (longer is more expensive)
+         temperature: 0.5, // randomness setting (can be set between 0 and 1)
       });
 
-      const openai = new OpenAIApi(config);
-
-      const generateFood = async () => {
-
-          const prompt = "As a chef, give a single suggestion and basic description of a food apporopriate for sharing at a casual cookout" //Change this to change AI input (longer is more expensive)
-
-         const response = await openai.createCompletion({
-              model: 'text-davinci-002', // language model, look up options to change (current option is cheapest text model)
-              prompt: prompt, 
-              max_tokens: 100,   // Output max length (longer is more expensive)
-             temperature: 0.5, // randomness setting (can be set between 0 and 1)
-          });
-
-         // console.log(response.data.choices[0].text); // uncomment this (1/2) to test
-          return response.data.choices[0].text
-      }
-
-
-      // generateFood(); // uncomment this (2/2) to test
-    }
+     // console.log(response.data.choices[0].text); // uncomment this (1/2) to test
+      return response.data.choices[0].string
   }
-})
+  // generateFood(); // uncomment this (2/2) to test
+}
